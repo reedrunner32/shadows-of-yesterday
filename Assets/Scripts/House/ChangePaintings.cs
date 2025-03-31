8c47cc2ce6554635f4059a710b5a830626934b25
Richard Zhang <82455544+reedrunner32@users.noreply.github.com>
2025-03-31 18:24:21 -0400

    fixed painting swap

--- Assets/Scripts/House/ChangePaintings.cs
+++ Assets/Scripts/House/ChangePaintings.cs
@@ -25,36 +25,39 @@ public class ChangePaintings : MonoBehaviour
 
     private void ChangePainting(GameObject interactedObject)
     {
-        if (painting1 != null && newTexture1 != null)
+        if (interactedObject == gameObject)
         {
-            Renderer renderer = painting1.GetComponent<Renderer>();
-            Material materialInstance = new Material(renderer.material);
-            materialInstance.SetTexture("_MainTex", newTexture1);
+            if (painting1 != null && newTexture1 != null)
+            {
+                Renderer renderer = painting1.GetComponent<Renderer>();
+                Material materialInstance = new Material(renderer.material);
+                materialInstance.SetTexture("_MainTex", newTexture1);
 
-            materialInstance.mainTextureScale = new Vector2(1.3f, 1.0f);
+                materialInstance.mainTextureScale = new Vector2(1.3f, 1.0f);
 
-            renderer.material = materialInstance;
-        }
-        if (painting2 != null && newTexture2 != null)
-        {
-            Renderer renderer = painting2.GetComponent<Renderer>();
-            Material materialInstance = new Material(renderer.material);
-            materialInstance.SetTexture("_MainTex", newTexture2);
-            renderer.material = materialInstance;
-        }
-        if (painting3 != null && newTexture3 != null)
-        {
-            Renderer renderer = painting3.GetComponent<Renderer>();
-            Material materialInstance = new Material(renderer.material);
-            materialInstance.SetTexture("_MainTex", newTexture3);
-            renderer.material = materialInstance;
-        }
-        if (painting4 != null && newTexture4 != null)
-        {
-            Renderer renderer = painting4.GetComponent<Renderer>();
-            Material materialInstance = new Material(renderer.material);
-            materialInstance.SetTexture("_MainTex", newTexture4);
-            renderer.material = materialInstance;
+                renderer.material = materialInstance;
+            }
+            if (painting2 != null && newTexture2 != null)
+            {
+                Renderer renderer = painting2.GetComponent<Renderer>();
+                Material materialInstance = new Material(renderer.material);
+                materialInstance.SetTexture("_MainTex", newTexture2);
+                renderer.material = materialInstance;
+            }
+            if (painting3 != null && newTexture3 != null)
+            {
+                Renderer renderer = painting3.GetComponent<Renderer>();
+                Material materialInstance = new Material(renderer.material);
+                materialInstance.SetTexture("_MainTex", newTexture3);
+                renderer.material = materialInstance;
+            }
+            if (painting4 != null && newTexture4 != null)
+            {
+                Renderer renderer = painting4.GetComponent<Renderer>();
+                Material materialInstance = new Material(renderer.material);
+                materialInstance.SetTexture("_MainTex", newTexture4);
+                renderer.material = materialInstance;
+            }
         }
     }
 }
